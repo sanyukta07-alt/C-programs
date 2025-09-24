e8afdd566d62d47c9b76b495db28abee5f2431dd
Sanyukta Ghosh <sanyukta.official07@gmail.com>
2025-09-24 08:35:45 +0530

    Update perimeter_area_given.cpp

--- perimeter_area_given.cpp
+++ perimeter_area_given.cpp
@@ -1,10 +1,13 @@
+//c program to compute the perimeter and area of a rectangle with a height of 7 inches and width of 5 inches.
+
 #include <stdio.h>
 
 int main() 
 {
     int height = 7;
     int width = 5;
-    int perimeter, area;
+    int perimeter = 0;
+	int area = 0;
 
     // Calculating perimeter and area
     perimeter = 2 * (height + width);
@@ -14,7 +17,7 @@ int main()
     printf("Height of rectangle = %d inches\n", height);
     printf("Width of rectangle  = %d inches\n", width);
     printf("Perimeter of rectangle = %d inches\n", perimeter);
-    printf("Area of rectangle      = %d square inches\n", area);
+    printf("Area of rectangle = %d square inches\n", area);
 
     return 0;
 }
