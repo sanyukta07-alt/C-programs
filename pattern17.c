3ebc235f7ccf0ba081cd96e97552b578c1655fb2
Sanyukta Ghosh <sanyukta.official07@gmail.com>
2026-08-14 11:06:45 +0530

    Add files via upload

---
+++ pattern17.c
@@ -0,0 +1,29 @@
+/*
+Enter the number of rows: 5
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+#include <stdio.h>
+
+void main()
+{
+    int i, j, k, n;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for (k = 1; k <= i; k++)
+            printf("*");
+
+        printf("\n");
+    }
+}
